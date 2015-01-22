9073f356d761fd2f18b80c5b6b809b24ea3b9869
John Sauter <John_Sauter@systemeyescomputerstore.com>
2015-01-22 16:52:45 -0500

    Add network commands on port 1500 to start a cluster, stop a cluster, and quit

---
+++ sound_effects_player/play_sound/src/parse_subroutines.h
@@ -0,0 +1,29 @@
+/*
+ * parse_subroutines.h
+ *
+ * Copyright © 2015 by John Sauter <John_Sauter@systemeyescomputerstore.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtk/gtk.h>
+
+/* Subroutines defined in parse_subroutines.c */
+
+/* Initialize the parser. */
+void *parse_init (GApplication *app);
+
+/* Accept text, divide or accumulate it into commands, 
+ * and execute those commands. */
+void parse_text (gchar *text, GApplication *app);
