6c879e329b5b0c7a824343e11371ef5009f61f53
John Sauter <John_Sauter@systemeyescomputerstore.com>
2015-02-15 23:53:45 -0500

    Make dependence on GTK+-3.14 clearer

--- sound_effects_player/play_sound/.anjuta/default.profile
+++ sound_effects_player/play_sound/.anjuta/default.profile
@@ -1,24 +1,24 @@
 <?xml version="1.0"?>
 <anjuta>
-    <plugin name="GNU Debugger" mandatory="no">
+    <plugin name="Terminal" mandatory="no">
         <require group="Anjuta Plugin"
                  attribute="Location"
-                 value="anjuta-gdb:GdbPlugin"/>
+                 value="anjuta-terminal:TerminalPlugin"/>
     </plugin>
     <plugin name="API Help" mandatory="no">
         <require group="Anjuta Plugin"
                  attribute="Location"
                  value="anjuta-devhelp:AnjutaDevhelp"/>
     </plugin>
-    <plugin name="Terminal" mandatory="no">
-        <require group="Anjuta Plugin"
-                 attribute="Location"
-                 value="anjuta-terminal:TerminalPlugin"/>
-    </plugin>
     <plugin name="Git" mandatory="no">
         <require group="Anjuta Plugin"
                  attribute="Location"
                  value="anjuta-git:Git"/>
+    </plugin>
+    <plugin name="GNU Debugger" mandatory="no">
+        <require group="Anjuta Plugin"
+                 attribute="Location"
+                 value="anjuta-gdb:GdbPlugin"/>
     </plugin>
     <plugin name="Code Snippets" mandatory="no">
         <require group="Anjuta Plugin"
--- sound_effects_player/play_sound/src/menu_handler.c
+++ sound_effects_player/play_sound/src/menu_handler.c
@@ -21,27 +21,24 @@
 #include <menu_handler.h>
 
 /* Subroutines used by the menus. */
-static void preferences_activated (GSimpleAction *action,
-				   GVariant *parameter,
-				   gpointer app)
+static void
+preferences_activated (GSimpleAction * action, GVariant * parameter,
+                       gpointer app)
 {
 }
 
 static void
-quit_activated (GSimpleAction *action,
-		GVariant *parameter,
-		gpointer app)
+quit_activated (GSimpleAction * action, GVariant * parameter, gpointer app)
 {
   g_application_quit (G_APPLICATION (app));
 }
 
 /* Actions table to link menu items to subroutines. */
-static GActionEntry app_entries[] =
-  {
-    { "preferences", preferences_activated, NULL, NULL, NULL },
-    { "quit", quit_activated, NULL, NULL, NULL }
-  };
-  
+static GActionEntry app_entries[] = {
+  {"preferences", preferences_activated, NULL, NULL, NULL},
+  {"quit", quit_activated, NULL, NULL, NULL}
+};
+
 /* Initialize the menu. */
 void
 play_sound_menu_init (GApplication * app, gchar * file_name)
@@ -50,16 +47,14 @@ play_sound_menu_init (GApplication * app, gchar * file_name)
   GError *error = NULL;
   GMenuModel *app_menu;
   GMenuModel *menu_bar;
-  
+
   const gchar *quit_accels[2] = { "<Ctrl>Q", NULL };
 
-  g_action_map_add_action_entries (G_ACTION_MAP (app),
-				   app_entries, G_N_ELEMENTS (app_entries),
-				   app);
-  gtk_application_set_accels_for_action (GTK_APPLICATION (app),
-					 "app.quit",
-					 quit_accels);
-  
+  g_action_map_add_action_entries (G_ACTION_MAP (app), app_entries,
+                                   G_N_ELEMENTS (app_entries), app);
+  gtk_application_set_accels_for_action (GTK_APPLICATION (app), "app.quit",
+                                         quit_accels);
+
   /* Load UI from file */
   builder = gtk_builder_new ();
   if (!gtk_builder_add_from_file (builder, file_name, &error))
@@ -77,8 +72,8 @@ play_sound_menu_init (GApplication * app, gchar * file_name)
   menu_bar = (GMenuModel *) gtk_builder_get_object (builder, "menubar");
   gtk_application_set_app_menu (GTK_APPLICATION (app), app_menu);
   gtk_application_set_menubar (GTK_APPLICATION (app), menu_bar);
-  
+
   g_object_unref (builder);
-  
+
   return;
 }
