2b0344a472390a0977f9ebe8a55de0c67011bbe1
macdroid53 <macdroid53@gmail.com>
2016-10-26 18:44:08 -0400

    lastest cueengine stuff

--- CueEngine/CueEngine.py
+++ CueEngine/CueEngine.py
@@ -388,7 +388,7 @@ if __name__ == "__main__":
     ui.disptext()
     ui.setfirstcue()
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
+    parser.add_argument("--ip", default="192.168.53.155", help="The ip of the OSC server")
     parser.add_argument("--port", type=int, default=5005, help="The port the OSC server is listening on")
     args = parser.parse_args()
 
--- CueEngine/TestProgLaunch.py
+++ CueEngine/TestProgLaunch.py
@@ -6,4 +6,7 @@ import os, sys, inspect, subprocess
 bla = subprocess.Popen(['python3', '/home/mac/PycharmProjs/linux-show-player/linux-show-player', '-f', '/home/mac/Shows/Pauline/sfx.lsp'])
 # /home/mac/PycharmProjs/linux-show-player/linux-show-player
 while 1:
-    pass
+    inkey = input('> ')
+    if inkey == 'q':
+        bla.terminate()
+        exit()
